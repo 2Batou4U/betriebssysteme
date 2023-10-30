bac7223113a91f33b064dd9b69638fca0534672b
adrian <adrian.klodzinski@gmx.de>
2023-10-30 12:09:53 +0100

    Fixed script.sh (added correct file names for compilations)

---
+++ prog00/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Aufgabe 2
+if [ -z "$1" ]
+then
+  echo "Fehlender Parameter"
+  exit 1
+elif [ "$1" == "compile" ]
+then
+  mkdir -p bin
+  gcc -std=c11 -Wall -Wextra -Werror -o bin/game main.c guess.c
+  echo Compile
+elif [ "$1" == "clean" ]
+then
+  rm -rf bin
+  echo Clean
+else
+  echo "Falscher Parameter"
+  exit 2
+fi
